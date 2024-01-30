bd8f6b7fd61b048dd0adda5a6e894044d587f141
Damian Wieclaw <1moka3@wp.pl>
2024-01-30 22:38:06 +0100

    Added screenshots

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+// o japierodleS
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
